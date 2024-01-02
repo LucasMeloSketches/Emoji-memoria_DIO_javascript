551b7eaf15c2002fd86103135c7dbd1adf0479ee
Lucas <lucaslook_ls@hotmail.com>
2024-01-02 10:56:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Emoji-memoria_DIO_javascript
+ Jogo da memória usando HTML, CSS e JavaScript.
---
+++ src/scripts/engine.js
@@ -0,0 +1,74 @@
+const emojis = [
+    "🤣",
+    "🤣",
+    "😑",
+    "😑",
+    "😱",
+    "😱",
+    "👻",
+    "👻",
+    "😬",
+    "😬",
+    "🥺",
+    "🥺",
+    "💩",
+    "💩",
+    "😎",
+    "😎"
+];
+let openCards = [];
+
+let tentativas = 10;
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.classList.add("boxOpen")
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+document.querySelector("#tentativas").innerHTML = `você tem ${tentativas} tentativas`;
+setTimeout(startGame, 500);
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+    if(openCards.length == 2){
+
+        setTimeout(checkMatch, 500);
+    }
+}
+function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else{
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+        if(tentativas > 0){
+            tentativas--;
+            document.querySelector("#tentativas").innerHTML = `você tem ${tentativas} tentativas`;
+        }else{
+            alert("Tente Novamente!")
+            for(let i = 0; i < document.querySelectorAll(".item").length; i++){
+                document.querySelectorAll(".item")[i].classList.add("boxOpen");;
+            }
+        }
+        
+    }
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Você Venceu!")
+    }
+}
+function startGame(){
+    for(let i = 0; i < document.querySelectorAll(".item").length; i++){
+        document.querySelectorAll(".item")[i].classList.remove("boxOpen");
+    }
+    
+}
